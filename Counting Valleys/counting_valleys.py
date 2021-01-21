5ddff1a99d36e03a675df6afab606958ac919e67
yashashri2102 <yashashri_naitam@persistent.co.in>
2021-01-21 07:44:18 +0530

    counting valleys solution

---
+++ Counting Valleys/counting_valleys.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import os
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+
+def countingValleys(steps, path):
+    # Write your code here
+    sea_level = 0
+    valley = 0
+
+    for i in path:
+        if i == 'U':
+            sea_level += 1
+            if sea_level == 0:
+                valley += 1
+        else:
+            sea_level -= 1
+    return valley
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    steps = int(input().strip())
+    path = input()
+    result = countingValleys(steps, path)
+    fptr.write(str(result) + '\n')
+    fptr.close()
